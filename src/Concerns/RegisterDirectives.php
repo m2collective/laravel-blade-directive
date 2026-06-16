dee311445f737f4370d7ab98f93f00097d1c6ecd
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-16 22:02:13 +0300

    Fix

--- src/Concerns/RegisterDirectives.php
+++ src/Concerns/RegisterDirectives.php
@@ -9,17 +9,17 @@ use M2Collective\BladeDirective\ClosingBladeDirective;
 use M2Collective\BladeDirective\ElseBladeDirective;
 use M2Collective\BladeDirective\OpeningBladeDirective;
 
-trait RegisterBladeDirectives
+trait RegisterDirectives
 {
     /**
      * @param array $directives
      * @return void
      */
-    private function registerBladeDirectives(array $directives) : void
+    private function registerDirectives(array $directives) : void
     {
         if($directives !== []) {
             foreach ($directives as $directive) {
-                $this->registerBladeDirective($directive);
+                $this->registerDirective($directive);
             }
         }
     }
@@ -28,7 +28,7 @@ trait RegisterBladeDirectives
      * @param BladeDirective $directive
      * @return void
      */
-    private function registerBladeDirective(BladeDirective $directive) : void
+    private function registerDirective(BladeDirective $directive) : void
     {
         if($directive instanceof OpeningBladeDirective) {
             Blade::directive($directive->openingTag(), [$directive, 'openingHandler']);
